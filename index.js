f8e3533179e898aa8def4ff8cdb0775c7c5cea2f
Brandon-k17 <br.ndonk17@gmail.com>
2026-05-13 11:19:40 +0100

    chore: bump version to v0.2.0

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-// AfriCode Hub - Point d'entrée de l'application console.log('AfriCode Hub v0.1.0 - Démarrage...');
+// AfriCode Hub - Point d'entrée de l'application console.log('AfriCode Hub v0.2.0 - Démarrage...');
